6040df360d67b2ccb23cd0e45ef1c76a7f2863d7
viswamac <viswa.vbd99@gmail.com>
2025-03-22 11:32:14 +0530

    Folder for FE

--- Porfolio_BE/obj/Debug/net8.0/Porfolio_BE.AssemblyInfo.cs
+++ Porfolio_BE/obj/Debug/net8.0/Porfolio_BE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Porfolio_BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af65ae21b64cada3cf8d94411d295c2caa6f1353")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b1155ebd7fc9fcb6f841a2fc897d08de1c8889d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Porfolio_BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Porfolio_BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
